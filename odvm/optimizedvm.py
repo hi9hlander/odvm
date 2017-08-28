76e915f89feefc8082ef29d313e2b114bd5cbe33
hi9hlander <30665937+hi9hlander@users.noreply.github.com>
2017-08-28 18:54:53 +0300

    (a) add different opt levels; (b) support for subtraction of voxels

--- odvm/optimizedvm.py
+++ odvm/optimizedvm.py
@@ -1,66 +1,6 @@
 from odvm.voxelmodel import VoxelModel
+from odvm.cuboids import cuboids_level, ijk1b_to_idx0_7, idx0_7_to_ijk1b, idx0_63_to_ijk2b
 from collections import defaultdict
-
-
-def ijk1b_to_idx0_7( i, j, k ):
-   assert( 0 <= i <= 1 and 0 <= j <= 1 and 0 <= k <= 1 )
-   return ((k&1)<<2)|((j&1)<<1)|(i&1)
-
-
-def idx0_7_to_ijk1b( idx ):
-   assert( 0 <= idx <= 7 )
-   return ( idx&1, (idx&2)>>1, (idx&4)>>2 )
-
-
-def ijk2b_to_idx0_63( i, j, k ):
-   assert( 0 <= i <= 3 and 0 <= j <= 3 and 0 <= k <= 3 )
-   return ( ((k&1)<<2)|((j&1)<<1)|(i&1) ) | ( ((k&2)<<4)|((j&2)<<3)|((i&2)<<2) )
-
-
-def idx0_63_to_ijk2b( idx ):
-   assert( 0 <= idx <= 63 )
-   return ( ((idx&8)>>2)|(idx&1), ((idx&16)>>3)|((idx&2)>>1), ((idx&32)>>4)|((idx&4)>>2) )
-
-
-class packed_cuboids(list):
-   def __init__(self):
-      list.__init__(self)
-      self.extend([0]*64)
-
-   def add( self, i, j, k ):
-      assert( 0 <= i <= 15 and 0 <= j <= 15 and 0 <= k <= 15 )
-      self[ijk2b_to_idx0_63(i>>2,j>>2,k>>2)] |= 1<<ijk2b_to_idx0_63(i&3,j&3,k&3)
-
-
-class cuboids_level(list):
-   def __init__( self, bits=8 ):
-      list.__init__(self)
-      self.extend([None]*64)
-      self.bits = bits
-
-   def add( self, i, j, k ):
-      idx = ijk2b_to_idx0_63(i>>self.bits,j>>self.bits,k>>self.bits)
-      if self[idx] is None:
-         if self.bits == 4: self[idx] = packed_cuboids()
-         else             : self[idx] = cuboids_level(self.bits-2)
-      mask = (1<<self.bits)-1
-      self[idx].add( i&mask, j&mask, k&mask )
-
-   def items( self ):
-      if self.bits == 4:
-         for idx,cs in enumerate(self):
-            if cs is not None:
-               i,j,k = idx0_63_to_ijk2b(idx)
-               yield ( (i<<4,j<<4,k<<4), cs )
-      else:
-         for idx,cs1 in enumerate(self):
-            if cs1 is not None:
-               i,j,k = idx0_63_to_ijk2b(idx)
-               i <<= self.bits
-               j <<= self.bits
-               k <<= self.bits
-               for ijk,cs in cs1.items():
-                  yield ( (i+ijk[0],j+ijk[1],k+ijk[2]), cs )
 
 
 class dict_cuboids_level(defaultdict):
@@ -68,20 +8,79 @@ class dict_cuboids_level(defaultdict):
 
 
 class OptimizedVM(VoxelModel):
-   def __init__( self, name, fmt ):
+   def __init__( self, name, fmt, level ):
       VoxelModel.__init__( self, name, fmt )
+      self.level   = level
       self.cuboids = ( dict_cuboids_level(), dict_cuboids_level(), dict_cuboids_level(), dict_cuboids_level(), 
                        dict_cuboids_level(), dict_cuboids_level(), dict_cuboids_level(), dict_cuboids_level() )
+      self.added   = ( dict_cuboids_level(), dict_cuboids_level(), dict_cuboids_level(), dict_cuboids_level(), 
+                       dict_cuboids_level(), dict_cuboids_level(), dict_cuboids_level(), dict_cuboids_level() )
+
+   def set_opt_level( self, level ): self.level = level
 
    def add(self,p2s,i,j,k,c,p2i=0,p2j=0,p2k=0):
+      ii  = 0 if i >= 0 else 1
+      ij  = 0 if j >= 0 else 1
+      ik  = 0 if k >= 0 else 1
+      idx = ijk1b_to_idx0_7(ii,ij,ik)
+      key = (c,1<<p2s,1<<p2i,1<<p2j,1<<p2k)
+      self.cuboids[idx][key].add(abs(i),abs(j),abs(k))
+
+      if   self.level == 0: VoxelModel.add(self,p2s,i,j,k,c,p2i,p2j,p2k)
+      elif self.level == 1: self.added[idx][key].add(abs(i),abs(j),abs(k))
+
+   def sub(self,p2s,i,j,k,p2i=0,p2j=0,p2k=0):
       ii = 0 if i >= 0 else 1
       ij = 0 if j >= 0 else 1
       ik = 0 if k >= 0 else 1
-      self.cuboids[ijk1b_to_idx0_7(ii,ij,ik)][(c,1<<p2s,1<<p2i,1<<p2j,1<<p2k)].add(abs(i),abs(j),abs(k))
+
+      s  = 1<<p2s
+      di = 1<<p2i
+      dj = 1<<p2j
+      dk = 1<<p2k
+
+      for key,cs in self.cuboids[ijk1b_to_idx0_7(ii,ij,ik)].items():
+         if key[1] == s and key[2] == di and key[3] == dj and key[4] == dk and cs.sub(abs(i),abs(j),abs(k)):
+            if   self.level == 0 or self.level == 1: # for level 1 keep list and process in optimize() call
+               cim = self.get(s,i-1,j,k,di,dj,dk)
+               if cim is None: cim = key[0]
+               cip = self.get(s,i+1,j,k,di,dj,dk)
+               if cip is None: cip = key[0]
+               cjm = self.get(s,i,j-1,k,di,dj,dk)
+               if cjm is None: cjm = key[0]
+               cjp = self.get(s,i,j+1,k,di,dj,dk)
+               if cjp is None: cjp = key[0]
+               ckm = self.get(s,i,j,k-1,di,dj,dk)
+               if ckm is None: ckm = key[0]
+               ckp = self.get(s,i,j,k+1,di,dj,dk)
+               if ckp is None: ckp = key[0]
+
+               self.quads.add( s,i,j,k,
+                  ( ( (  0,  0,  0,  0, dj,-dk ), cim ),
+                    ( ( di,  0,-dk, di, dj,  0 ), cip ),
+                    ( (  0,  0,  0, di,  0,-dk ), cjm ),
+                    ( (  0, dj,-dk, di, dj,  0 ), cjp ),
+                    ( (  0, dj,  0, di,  0,  0 ), ckm ),
+                    ( (  0,  0,-dk, di, dj,-dk ), ckp ) ) )
+            return
+
+   def get(self,s,i,j,k,di,dj,dk):
+      ii = 0 if i >= 0 else 1
+      ij = 0 if j >= 0 else 1
+      ik = 0 if k >= 0 else 1
+      for key,cs in self.cuboids[ijk1b_to_idx0_7(ii,ij,ik)].items():
+         if key[1] == s and key[2] == di and key[3] == dj and key[4] == dk and cs.get(abs(i),abs(j),abs(k)): return key[0]
+      else: return None
 
    def optimize(self):
+      if self.level == 0: return
       with self.quads:
-         for idx2,cs2 in enumerate(self.cuboids):
+         if   self.level == 1: cuboids = self.added
+         elif self.level == 2:
+            self.quads.reset()
+            cuboids = self.cuboids
+         else: assert( 0 <= self.level <= 2 )
+         for idx2,cs2 in enumerate(cuboids):
             si,sj,sk = idx0_7_to_ijk1b(idx2)
             si = 1-si-si
             sj = 1-sj-sj
@@ -107,3 +106,4 @@ class OptimizedVM(VoxelModel):
                                 ( ( 0, dj,-dk, di,  0,-dk ), c ),
                                 ( ( 0, dj,  0, di, dj,-dk ), c ) ) )
                         m += m
+                     if self.level == 1: cs[idx] = 0
